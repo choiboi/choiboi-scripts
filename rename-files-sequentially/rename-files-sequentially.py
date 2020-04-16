a0925981e42ce7ec2db784083fc1adc18b6e2636
James Choi <james.choiboi@gmail.com>
2020-04-15 20:56:24 -0400

    Add better verification.

--- rename-files-sequentially/rename-files-sequentially.py
+++ rename-files-sequentially/rename-files-sequentially.py
@@ -32,7 +32,9 @@ def verify_provided_inputs(from_dir, to_dir, filename_prefix, starting_sequence,
         return False
 
     try:
-        int(seq_num_digits)
+        seq_num_digits = int(seq_num_digits)
+        if seq_num_digits < 1 or seq_num_digits > 10:
+            print "Number digits for the sequence number must be an Integer must be between 1 to 10."
     except ValueError:
         print "Number digits for the sequence number must be an Integer."
         return False
@@ -44,6 +46,6 @@ if __name__ == '__main__':
     to_dir = ask_for_input("Enter directory where you want to move the files to.\n")
     filename_prefix = ask_for_input("Specify new filename prefix.\n")
     starting_sequence = ask_for_input("Specify starting sequence number.\n")
-    seq_num_digits = ask_for_input("Number of digits for the sequence number.\n")
+    seq_num_digits = ask_for_input("Number of digits for the sequence number between (1 to 10).\n")
 
     isResponsesValid = verify_provided_inputs(from_dir, to_dir, filename_prefix, starting_sequence, seq_num_digits)
